f5653c8fea5708e2fd98a2f64f05b0cd6c45b4f7
Mogilevskiy Vladislav <mogilevskiy499@gmail.com>
2021-03-04 01:32:56 +0300

    First try

---
+++ Assets/Group1/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private float _range;
+
+    private Vector3 _target;
+
+    private void Start()
+    {
+        SetNextTarget();
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _target, _speed * Time.deltaTime);
+
+        if (transform.position == _target)
+            SetNextTarget();
+    }
+
+    private void SetNextTarget()
+    {
+        _target = Random.insideUnitCircle * _range;
+    }
+}
